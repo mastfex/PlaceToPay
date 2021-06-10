0ff63386377614a8187b2fe14b095c53f598e14b
Christian Andres Ramirez Armijo <caramirez@vectoritcgroup.com>
2021-06-10 16:27:55 -0400

    Cambios

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.DemoService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.service.DemoService;
 
 @RestController
 @RequestMapping("demo")
--- src/main/java/com/example/demo/service/metodo/DemoTokenize.java
+++ src/main/java/com/example/demo/service/metodo/DemoTokenize.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.service.metodo;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
