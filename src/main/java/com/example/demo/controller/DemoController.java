627ad686bf45c720f2021c2d6f4600160bcbaa0f
mastfex <frubin44@gmail.com>
2021-06-10 16:16:30 -0400

    Se refactorizan clases

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.DemoService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DemoService;
 
 @RestController
 @RequestMapping("demo")
@@ -25,6 +27,21 @@ public class DemoController {
          @RequestParam(name = "nombre", required = true) String Nombre
     ){
         return service.Template(Nombre);
+    }
+    
+    @PostMapping("/tokenize")
+    public com.example.demo.request.tokenize.Input tokenizer(
+         @RequestBody com.example.demo.request.tokenize.Input input
+    ){
+    	
+        return input;
+    }
+    
+    @PostMapping("/process")
+    public com.example.demo.request.transaction.Input processTransaction(
+         @RequestBody com.example.demo.request.transaction.Input input
+    ){
+        return input;
     }
 
 }
--- src/main/java/com/example/demo/service/DemoService.java
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -1,8 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.request.tokenize.Card;
+import com.example.demo.request.tokenize.Input;
+import com.example.demo.request.tokenize.Instrument;
+import com.example.demo.request.tokenize.Payer;
+import com.example.demo.response.tokenize.Output;
+import com.example.demo.utils.GenerateAuth;
 import com.fasterxml.jackson.core.sym.Name;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class DemoService {
@@ -10,5 +19,40 @@ public class DemoService {
 
     public String Template(String Name){
         return "¡Hola " + Name + "!";
+    }
+    
+    public com.example.demo.response.tokenize.Output postTokenize(com.example.demo.request.tokenize.Input input){
+    	com.example.demo.response.tokenize.Output output = new com.example.demo.response.tokenize.Output();
+    	
+    	Instrument instrument = new Instrument();
+		Card card=new Card();
+		Payer payer = new Payer();
+		
+		GenerateAuth oauth=new GenerateAuth();
+		input.setAuth(oauth.getAuth());
+		card.setNumber(input.getInstrument().getCard().getNumber());
+		card.setCvv(input.getInstrument().getCard().getCvv());
+		card.setExpirationMonth(input.getInstrument().getCard().getExpirationMonth());
+		card.setExpirationYear((input.getInstrument().getCard().getExpirationYear()));   
+		instrument.setCard(card);
+		instrument.setOtp("");
+		input.setInstrument(instrument);
+		input.setIpAddress(input.getIpAddress());
+		payer.setName(input.getPayer().getName());
+		payer.setSurname(input.getPayer().getSurname());
+		payer.setEmail(input.getPayer().getEmail());
+		input.setPayer(payer);
+		input.setUserAgent(input.getUserAgent());
+		
+		WebClient clientFlux = WebClient.builder().baseUrl("https://test.placetopay.ec/rest").build();
+		clientFlux.post().uri(uriBuilder -> uriBuilder.path("/gateway/tokenize").build())
+				.body(Mono.just(input), Input.class)
+				.retrieve()
+				.bodyToMono(Output.class)
+				.log()
+				.block();
+    	
+    	output = (Output) clientFlux;
+    	return output;
     }
 }
